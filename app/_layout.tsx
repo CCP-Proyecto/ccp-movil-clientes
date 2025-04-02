d11c0119dbe03708a75a4c422d9147a4abc18dd4
Nicolas Rey <nicolasreydh@gmail.com>
2025-04-02 13:08:16 -0500

    feat: add initial structure for authentication and UI components, fonts and toast

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,55 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import Toast from "react-native-toast-message";
+
+import { authClient } from "@/services/auth/auth-client";
 
 export default function RootLayout() {
-  return <Stack />;
+  const { isPending, data: session } = authClient.useSession();
+
+  const [fontsLoaded, fontError] = useFonts({
+    "Comfortaa-Light": require("../assets/fonts/Comfortaa-Light.ttf"),
+    "Comfortaa-Regular": require("../assets/fonts/Comfortaa-Regular.ttf"),
+    "Comfortaa-Medium": require("../assets/fonts/Comfortaa-Medium.ttf"),
+    "Comfortaa-SemiBold": require("../assets/fonts/Comfortaa-SemiBold.ttf"),
+    "Comfortaa-Bold": require("../assets/fonts/Comfortaa-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      // Ocultar la pantalla de splash cuando las fuentes estén cargadas o haya un error
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  // Devolver null hasta que las fuentes estén cargadas
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  if (isPending) {
+    return null; // TODO: Hacer un loading
+  }
+
+  // Devolver null hasta que las fuentes estén cargadas
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        {!session ? (
+          // Rutas para usuarios no autenticados
+          <Stack.Screen name="(auth)" />
+        ) : (
+          // Rutas para usuarios autenticados
+          <Stack.Screen name="(app)" />
+        )}
+      </Stack>
+      <Toast />
+    </>
+  );
 }
